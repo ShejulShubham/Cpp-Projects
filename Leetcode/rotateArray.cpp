8c68619030169c9cf087fdeec092ee3d319d7816
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-12-26 18:58:43 +0530

    Added reverse function

--- Leetcode/rotateArray.cpp
+++ Leetcode/rotateArray.cpp
@@ -13,18 +13,21 @@ public:
 
     }
 
-    void solution1(std::vector<int>& nums, int k) {
+    void solution1(std::vector<int>& nums, int k) {//Input: (nums = {1, 2, 3, 4, 5}, k = 2)
         int n = nums.size();
         k = k % n; // Reduce k to prevent unnecessary rotations
         
         // Reverse the entire array
-        std::reverse(nums.begin(), nums.end());
+        Solution::reverse(nums, 0, nums.size());
+        //output: {5, 4, 3, 2, 1}
         
         // Reverse the first k elements
-        std::reverse(nums.begin(), nums.begin() + k);
+        Solution::reverse(nums, 0, k);
+        //output: {4, 5, 3, 2, 1}
         
         // Reverse the remaining n-k elements
-        std::reverse(nums.begin() + k, nums.end());
+        Solution::reverse(nums, k, nums.size());
+        //output: {4, 5, 1, 2, 3}
     }
 
     void solution2(vector<int>& nums, int k){
@@ -34,6 +37,19 @@ public:
             pos = nums.insert(pos, nums.back());
 
             nums.pop_back();
+        }
+    }
+
+private:
+    void reverse(vector<int>& container, int start, int end){
+
+        while(start < end){
+            int temp = container[start];
+            container[start] = container[end];
+            container[end] = temp;
+
+            start++;
+            end--;
         }
     }
 };
