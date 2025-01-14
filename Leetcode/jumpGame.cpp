c9495b9ef3b75b8a53216a3335362b3aef0a2352
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-01-14 17:36:23 +0530

    leetcode - 55. Jump Game

---
+++ Leetcode/jumpGame.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+// leetcode - 55. Jump Game
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+
+        int maxReach = 0;
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+
+            if(i > maxReach)
+                return false;
+
+            maxReach = max(maxReach, i + nums[i]);
+
+            if(maxReach >= n - 1)
+                return true;
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+
+    vector<int> *list = new vector<int>({2, 0});
+
+    Solution solve;
+
+    string result = solve.canJump(*list) ? "TRUE" : "FALSE";
+
+    cout << "output: " << result << endl;
+
+    return 0;
+}
