f2f29d7532915e643dc796bc5891b1dc3c3ddd85
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2025-01-31 13:01:12 +0530

    Question: Prime number

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -3,32 +3,40 @@
 
 using namespace std;
 
-int main(){
-    int limit = 100; // set the limit 
-    vector<int>* list = new vector<int>();
+bool isPrimeNumber(int n){
     int count = 0;
 
-    list->push_back(2);
-    list->push_back(3);
-    list->push_back(5);
-    list->push_back(7);
-
-
-    for(int i = 11; i < limit; i++){
-        if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
-            continue;
-        }else{
-            list->push_back(i);
-
+    for(int i = 1; i <= n; i++){
+        if(n%i == 0)
             count++;
-        }
     }
 
-    for(auto n : *list){
-        cout << n << ", ";
-    }
+    if(count == 2)
+        return true;
 
-    cout << endl << "Prime numbers: " << list->size() << endl;
+    return false;
+}
+
+int main(){
+    int limit = 100; // set the limit 
+    vector<int>* list = new vector<int>({2, 3, 4, 5, 6, 7, 8, 9});
+    
+
+
+    for(auto& n : *list){
+        cout << "Is " << n << " Prime Number? ";
+        cout<< (isPrimeNumber(n) ? "YES" : "NO") << endl;
+    }
+    // FOLLOWING METHOD DOES NOT WORK
+    // for(int i = 11; i < limit; i++){
+    //     if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+    //         continue;
+    //     }else{
+    //         list->push_back(i);
+
+    //         count++;
+    //     }
+    // }
 
 
     return 0;
